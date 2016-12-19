3cb6d659edfeb9f330dca6cf6617426f8eea30a6
othman853 <anuar.yasser@gmail.com>
2016-12-19 13:42:32 -0200

    Adding items to the list on the button click

--- app/src/main/java/io/github/othman853/kotlinapp/MainActivity.kt
+++ app/src/main/java/io/github/othman853/kotlinapp/MainActivity.kt
@@ -2,10 +2,8 @@ package io.github.othman853.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Toast
-import retrofit2.Retrofit
+import android.widget.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retro = Retrofit.Builder().build()
-
         val lista = findViewById(R.id.lista) as ListView
+        val addItemBtn = findViewById(R.id.btn_add_item) as Button
+        val addItemEditText = findViewById(R.id.etxt_item) as EditText
 
-        Toast.makeText(this, "asdadasd", Toast.LENGTH_SHORT).show()
-
-        lista.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOf("a", "b", "c"))
+        lista.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ArrayList<String>())
 
         lista.setOnItemClickListener { adapterView, view, i, l -> Toast.makeText(this, "Oi", Toast.LENGTH_SHORT).show() }
 
+        addItemBtn.setOnClickListener { view -> (lista.adapter as ArrayAdapter<String>).add(addItemEditText.text.toString()) }
     }
 
 }
